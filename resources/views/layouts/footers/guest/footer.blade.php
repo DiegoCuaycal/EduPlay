0eea1d5766ee10bdf9ed16b3be27d6f03e59919f
Ernesto <102611496+DiegoCuaycal@users.noreply.github.com>
2024-09-02 10:29:36 -0500

    Login and Register V2

--- resources/views/layouts/footers/guest/footer.blade.php
+++ resources/views/layouts/footers/guest/footer.blade.php
@@ -2,6 +2,7 @@
   <footer class="footer py-5">
     <div class="container">
       <div class="row">
+        <!--
       <div class="col-lg-8 mb-4 mx-auto text-center">
           <a href="https://www.creative-tim.com/?_ga=2.242299972.757293697.1638911086-1528502635.1638911086" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
               Company
@@ -22,13 +23,14 @@
               Pricing
           </a>
       </div>
+      <-->
         @if (!auth()->user() || \Request::is('static-sign-up')) 
           <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
               <a href="https://dribbble.com/creativetim" target="_blank" class="text-secondary me-xl-4 me-4">
                   <span class="text-lg fab fa-dribbble" aria-hidden="true"></span>
               </a>
               <a href="https://twitter.com/CreativeTim" target="_blank" class="text-secondary me-xl-4 me-4">
-                  <span class="text-lg fab fa-twitter" aria-hidden="true"></span>
+                  <span class="text-lg fab fa-facebook" aria-hidden="true"></span>
               </a>
               <a href="https://www.instagram.com/creativetimofficial/" target="_blank" class="text-secondary me-xl-4 me-4">
                   <span class="text-lg fab fa-instagram" aria-hidden="true"></span>
@@ -46,12 +48,14 @@
         <div class="row">
           <div class="col-8 mx-auto text-center mt-1">
             <p class="mb-0 text-secondary">
-              Copyright © <script>
+              Derechos de autor © <script>
                 document.write(new Date().getFullYear())
-              </script> Soft by 
+              </script> Soft  
+              <!--
               <a style="color: #252f40;" href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
               &
               <a style="color: #252f40;" href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">UPDIVISION</a>.
+-->
             </p>
           </div>
         </div>
