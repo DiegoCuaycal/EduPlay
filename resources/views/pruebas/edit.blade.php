a016b6d4b33737cac38a4c05454f28cb8289cdc5
FerroCenter <diegocuaycal01@gmail.com>
2024-10-16 20:07:41 -0500

    Modificacion la vista de editar

--- resources/views/pruebas/edit.blade.php
+++ resources/views/pruebas/edit.blade.php
@@ -7,8 +7,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="container">
-        <h2>Editar Prueba: {{ $prueba->titulo }}</h2>
+    <div class="container mt-5 p-4 shadow bg-white rounded">
+        <h2 class="text-center mb-4">Editar Prueba: {{ $prueba->titulo }}</h2>
 
         <form action="{{ route('pruebas.update', $prueba->id) }}" method="POST">
             @csrf
@@ -21,23 +21,30 @@
 
             <div id="preguntas">
                 @foreach ($prueba->preguntas as $index => $pregunta)
-                    <div class="form-group">
-                        <h4>Pregunta {{ $index + 1 }}</h4>
-                        <input type="text" class="form-control" name="preguntas[{{ $pregunta->id }}]" value="{{ $pregunta->texto }}" required>
+                    <div class="form-group border p-3 mb-4 rounded">
+                        <h4 class="text-primary">Pregunta {{ $index + 1 }}</h4>
+                        <input type="text" class="form-control mb-3" name="preguntas[{{ $pregunta->id }}]" value="{{ $pregunta->texto }}" required>
 
                         <label>Respuestas</label>
                         <div class="form-group">
                             @foreach ($pregunta->respuestas as $respuesta)
-                                <input type="text" class="form-control" name="respuestas[{{ $respuesta->id }}]" value="{{ $respuesta->texto }}" required>
-                                <input type="checkbox" name="correctas[{{ $pregunta->id }}][]" value="{{ $respuesta->id }}" {{ $respuesta->es_correcta ? 'checked' : '' }}> Correcta
+                                <div class="input-group mb-2">
+                                    <input type="text" class="form-control" name="respuestas[{{ $respuesta->id }}]" value="{{ $respuesta->texto }}" required>
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <input type="checkbox" name="correctas[{{ $pregunta->id }}][]" value="{{ $respuesta->id }}" {{ $respuesta->es_correcta ? 'checked' : '' }}> Correcta
+                                        </div>
+                                    </div>
+                                </div>
                             @endforeach
                         </div>
                     </div>
                 @endforeach
             </div>
 
-            <button type="submit" class="btn btn-primary">Guardar Cambios</button>
+            <button type="submit" class="btn btn-primary btn-block">Guardar Cambios</button>
         </form>
     </div>
 </body>
 </html>
+
