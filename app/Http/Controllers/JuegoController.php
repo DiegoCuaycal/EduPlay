ab413f250548c26c47957be333c4df197e5432d9
FerroCenter <diegocuaycal01@gmail.com>
2024-11-17 16:16:54 -0500

    Pruebas de usuario dentro del boton jugar

--- app/Http/Controllers/JuegoController.php
+++ app/Http/Controllers/JuegoController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class JuegoController extends Controller
 {
-    //
+    public function jugar(Request $request)
+    {
+        // Validar la URL ingresada en el formulario
+        $request->validate([
+            'gameUrl' => 'required|url'
+        ]);
+
+        // Redirigir a la URL ingresada
+        return redirect()->to($request->input('gameUrl'));
+    }
 }
