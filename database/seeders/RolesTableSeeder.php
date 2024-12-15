42419c021ade018c1448ab141f8e7acc0ab20d73
FerroCenter <diegocuaycal01@gmail.com>
2024-12-15 12:52:39 -0500

    Modificacion de las migraciones

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -10,8 +10,9 @@ class RolesTableSeeder extends Seeder
     public function run()
     {
         DB::table('roles')->insert([
-            ['id' => 1, 'name' => 'Admin', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 2, 'name' => 'User', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 1, 'name' => 'Admin', 'guard_name' => 'web', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 2, 'name' => 'User', 'guard_name' => 'web', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
+
