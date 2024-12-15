3f25ae2d462e3d7a11d3225a3d02152046166f4c
Gateons <nattmejia1@gmail.com>
2024-12-15 00:31:41 -0500

    Sidebar update

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -108,10 +108,10 @@ document.getElementById('inicio-link').addEventListener('click', function (e) {
 
   @endif
 
-    <!-- Opciones para el rol 'admin' -->
+    <!-- Opciones para el rol 'admin' laravel examples-->
     @if (Auth::check() && Auth::user()->hasRole('Admin'))
     <li class="nav-item mt-2">
-      <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Laravel Examples</h6>
+      <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6"></h6>
     </li>
     <li class="nav-item">
       <a class="nav-link {{ (Request::is('dashboard') ? 'active' : '') }}" href="{{ url('dashboard') }}">
@@ -140,21 +140,9 @@ document.getElementById('inicio-link').addEventListener('click', function (e) {
       <span class="nav-link-text ms-1">Inicio</span>
       </a>
     </li>
-
-    <li class="nav-item pb-2">
-      <a class="nav-link {{ (Request::is('user-management') ? 'active' : '') }}" href="{{ url('user-management') }}">
-      <div
-        class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-        <i style="font-size: 1rem;"
-        class="fas fa-lg fa-list-ul ps-2 pe-2 text-center text-dark {{ (Request::is('user-management') ? 'text-white' : 'text-dark') }} "
-        aria-hidden="true"></i>
-      </div>
-      <span class="nav-link-text ms-1">User Management</span>
-      </a>
-      </li>
-
+     
       <li class="nav-item mt-2">
-      <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Biblioteca</h6>
+      <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Actividades</h6>
       </li>
       <li class="nav-item">
       <a class="nav-link {{ (Request::is('tables') ? 'active' : '') }}" href="{{ url('tables') }}">
@@ -179,50 +167,10 @@ document.getElementById('inicio-link').addEventListener('click', function (e) {
         </g>
         </svg>
       </div>
-      <span class="nav-link-text ms-1">Informes</span>
-      </a>
-    </li>
-    <li class="nav-item pb-2">
-      <a class="nav-link {{ (Request::is('user-management') ? 'active' : '') }}" href="{{ url('user-management') }}">
-      <div
-        class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-        <i style="font-size: 1rem;"
-        class="fas fa-lg fa-list-ul ps-2 pe-2 text-center text-dark {{ (Request::is('user-management') ? 'text-white' : 'text-dark') }} "
-        aria-hidden="true"></i>
-      </div>
-      <span class="nav-link-text ms-1">User Management</span>
+      <span class="nav-link-text ms-1">Crear Actividades</span>
       </a>
       </li>
-
-      <li class="nav-item mt-2">
-      <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Biblioteca</h6>
-      </li>
-      <li class="nav-item">
-      <a class="nav-link {{ (Request::is('tables') ? 'active' : '') }}" href="{{ url('tables') }}">
-      <div
-        class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-        <svg width="12px" height="100%" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink">
-        <title>office</title>
-        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g transform="translate(-1869.000000, -293.000000)" fill="#FFFFFF" fill-rule="nonzero">
-        <g transform="translate(1716.000000, 291.000000)">
-          <g id="office" transform="translate(153.000000, 2.000000)">
-          <path class="color-background opacity-6"
-          d="M12.25,17.5 L8.75,17.5 L8.75,1.75 C8.75,0.78225 9.53225,0 10.5,0 L31.5,0 C32.46775,0 33.25,0.78225 33.25,1.75 L33.25,12.25 L29.75,12.25 L29.75,3.5 L12.25,3.5 L12.25,17.5 Z">
-          </path>
-          <path class="color-background"
-          d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 L12.25,36.75 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 L12.25,29.75 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 L35,36.75 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 L35,29.75 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 L35,22.75 Z">
-          </path>
-          </g>
-        </g>
-        </g>
-        </g>
-        </svg>
-      </div>
-      <span class="nav-link-text ms-1">Informes</span>
-      </a>
-      </li>
+      
       <li class="nav-item">
       <a class="nav-link {{ (Request::is('pruebas.cuadros') ? 'active' : '') }}" href="{{ url('pruebas/cuadros') }}">
       <div
@@ -246,7 +194,7 @@ document.getElementById('inicio-link').addEventListener('click', function (e) {
         </g>
         </svg>
       </div>
-      <span class="nav-link-text ms-1">Pruebas</span>
+      <span class="nav-link-text ms-1">Ver Actividades</span>
       </a>
       </li>
       <li class="nav-item">
@@ -272,7 +220,7 @@ document.getElementById('inicio-link').addEventListener('click', function (e) {
               </g>
             </svg>
           </div>
-          <span class="nav-link-text ms-1">Pruebas Realizadas</span>
+          <span class="nav-link-text ms-1">Actividades Realizadas</span>
         </a>
       </li>
 
@@ -305,7 +253,7 @@ document.getElementById('inicio-link').addEventListener('click', function (e) {
       <li class="nav-item"></li>
 
       <li class="nav-item mt-3">
-      <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
+      <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Cuenta</h6>
       </li>
       <li class="nav-item">
       <a class="nav-link {{ (Request::is('profile') ? 'active' : '') }}" href="{{ url('profile') }}">
@@ -333,7 +281,7 @@ document.getElementById('inicio-link').addEventListener('click', function (e) {
         </g>
         </svg>
       </div>
-      <span class="nav-link-text ms-1">Profile</span>
+      <span class="nav-link-text ms-1">Perfil</span>
       </a>
       </li>
 
